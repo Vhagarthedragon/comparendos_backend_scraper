71c39f83e2b672ed4f336c444d592f0f49b45f05
Daniel Diaz <fer_cesar98@hotmail.com>
2023-03-22 17:23:48 -0500

    cambios en consulta masiva y add endpoint para crm

--- infraction_core/core_system/urls.py
+++ infraction_core/core_system/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from . import consultamasiva, views
 
 urlpatterns = [
-    path('comparendos/', views.Fotomultas.as_view(), name='comparendos'),
-    path('comparendosconsulta/', views.FotomultasConsulta.as_view(), name='comparendosconsulta'),
+    path('comparendos/', views.Fotomultas.as_view(), name='comparendos'), #consulta de comparendos a la bd
+    path('comparendosconsulta/', views.FotomultasConsulta.as_view(), name='comparendosconsulta'), #consulta de comparendos al scraper
+    path('consultamasiva/', consultamasiva.Multas.as_view(), name='consultamasiva'), #consulta masiva de comparendos
+    path('ComparendosCrm/', views.ComparendosCrm.as_view(), name='ComparendosCrm'), #consulta de comparendos sin thread
 ]

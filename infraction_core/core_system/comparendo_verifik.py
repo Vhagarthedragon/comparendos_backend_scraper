303b2cd120841a98bfc6a4f179b3d19554627169
Daniel Diaz <fer_cesar98@hotmail.com>
2023-03-24 16:59:00 -0500

    implementacion consulta thread scrapers

--- infraction_core/core_system/comparendo_verifik.py
+++ infraction_core/core_system/comparendo_verifik.py
@@ -27,9 +27,9 @@ class ComparendoVerifik(IVerifik):
     """
     def __init__(self) -> None:
         self.origin = None
-        self.__endpoints = ['http://127.0.0.1:8000/scraper-simit/',
-                            'http://127.0.0.1:8000/scraper-cali/',
-                            'http://127.0.0.1:8000/scraper-medellin/',]
+        self.__endpoints = ['http://ec2-34-199-206-42.compute-1.amazonaws.com/scraper-simit/',
+                            'http://ec2-34-199-206-42.compute-1.amazonaws.com/scraper-cali/',
+                            'http://ec2-34-199-206-42.compute-1.amazonaws.com/scraper-medellin/',]
         self.__customer = None
         self.__comparendos_obj = {'comparendos': list(), 'resoluciones': list()}
         
